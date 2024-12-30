242b7d82a77815536a132590df5d8502e2e5ea63
Natan Xavier <55638524+natan-xav2019@users.noreply.github.com>
2024-12-30 16:38:55 -0300

    Feat: Accordion animation

---
+++ assets/js/accordion.js
@@ -0,0 +1,16 @@
+const accordionTrigger = document.querySelectorAll('.accordion .trigger')
+
+accordionTrigger.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+        
+        const accordion = trigger.parentElement
+        const isOpen = accordion.classList.contains('open')
+
+        if(isOpen) {
+            accordion.classList.remove("open")
+        }
+        else {
+            accordion.classList.add("open")
+        }
+    })
+})
